815b6868f5281ca2911a5ab38cbe06ac2b12e7d1
Eric <ebagnaro@gmail.com>
2024-04-23 13:31:34 +0200

    Initial Commit

---
+++ FacebookTutorial/FacebookTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookTutorialApp.swift
+//  FacebookTutorial
+//
+//  Created by Eric on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FacebookTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
